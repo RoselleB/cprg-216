bbd258ea6c56c380142ba65c5312dc124bcf95e2
RoselleB <Roselleblanco@hotmail.com>
2025-09-23 11:23:47 -0600

    Practice for Week4

---
+++ Practice.py
@@ -0,0 +1,18 @@
+print("Welcome to Object-Orienting Program")
+User_Input = input("What it your name? ")
+print("Hello,", User_Input)
+
+User_age = input("what is your age? ")
+age = int(User_age,)
+print(age,  end=",")
+What_year = (2025 - age)
+print(What_year)
+is_adult = (age <= 18)
+if is_adult:
+    print("Congrats on being an adult")
+print(is_adult)
+
+
+
+
+
