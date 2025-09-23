9c766fa09c3d5e96023f2bcacce8096be033eb85
RoselleB <Roselleblanco@hotmail.com>
2025-09-23 08:20:57 -0600

    Week 3 demo

---
+++ week3-demo.py
@@ -0,0 +1,54 @@
+print("Welcome to the Age Calculator app..")
+user_input = input("what is you name")
+name = user_input
+print("Hello,", name)
+# year = input("What is your year of birth?")Error
+# age = 2025 - year Error - as it trying to minus a 
+# int with a string of text
+#print(age)
+#user_input = input("What is your year of birth?")
+#year = int(user_input)
+#age = 2025 - year
+#print(age)
+
+#operators 
+# + - ? *
+x = 3+4
+y = 3*4
+z = 3/4
+t= 4-3
+
+u = t - 1
+# the remainder %
+
+v =  4%3 # 1
+x = 5//3 #1 int(5/3)
+y = 2**3 # power 2*2*2
+
+print(v, x, y)
+
+# augmented operation
+x = x+3 #4
+y *= 2
+print(x)
+print(y)
+y /= 8
+print(y)
+y -= 2
+print(y)
+y %= 2
+
+#when we try to write something that is large
+#we go to the next
+
+# print function in detail
+
+#print = a stream of character( an output stream)
+#float = when you add to number to together and one is int and the other is float the number becomes float
+#sep = seperator = space
+#end = end make it so the line below where u put end is on the same line
+print("hello", "world", sep=',')
+
+print("hello", "world", end='/t')
+print("hello", "world", end=',/t')
+print("hello", "world")
