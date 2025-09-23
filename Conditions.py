ddd17bf532a510f66ad4a2d76f44a6f25578d30c
RoselleB <Roselleblanco@hotmail.com>
2025-09-23 10:24:46 -0600

    Week 4

---
+++ Conditions.py
@@ -0,0 +1,39 @@
+
+# Try to choose a variable namef or your boolean expression in 
+#form of a question
+
+#Boolean Expression
+is_old = True
+has_one = False
+isMale = True
+isChild = False
+
+#False
+expression = 3 > 4
+
+print("Hello to the party program")
+user_input =input("Please enter your age: ")
+age = int(user_input)
+
+is_adult = age >= 18
+
+print(is_adult)
+# == is a question is 3 = 4
+# comparison operators 
+# equality ==
+# larger than > 
+# larger than or equal >=
+# less than or equal <=
+# example given a, b, c
+
+
+a = 0
+b = 3
+c= 4
+print(b**2 - 4*a*c >=0)
+
+if is_adult: 
+    print("yes, is adult") #this line will be ignored if condition is False18
+
+
+
---
+++ Week_4.py
@@ -0,0 +1,6 @@
+# a variable has a type, size, name, adress, value
+# Casting changing a variable value from float or Int or something else
+# print("hello") does not have value so it cannot be stored in a variable
+# mathematic equations if the operation is equal it is read left to right
+# \t tab space \b backspace \n new line \\ converges on \ into one
+
